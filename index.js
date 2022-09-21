089c9576b71a61eb6435326e1a699a8133530a3e
Your Name <you@example.com>
2022-09-21 15:27:18 -0400

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//var debug = require('debug')('frontend-code-challenge');
+var debug = require('debug')('frontend-code-challenge');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('./lib/logger');
-const nunjucks = require("nunjucks");
+var cors = require('cors');
 
 var users = require('./routes/users');
 
@@ -16,13 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/users', users);
 
-nunjucks.configure("templates", {
-  autoescape: true,
-  express: app
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -3,87 +3,59 @@ var router = express.Router();
 var _ = require('lodash');
 var logger = require('../lib/logger');
 var log = logger();
-const User = require("../models/user");
+//const User = require("../models/user");
 const { createToken } = require("../token");
 
-//var users = require('../init_data.json').data;
-//var curId = _.size(users);
+var users = require('../init_data.json').data;
+var curId = _.size(users);
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  try{
-    const users = await User.findAll();
-    return res.json({ users });
-  } catch(err){
-    return next(err);
-  }
-  // res.json(_.toArray(users));
+router.get('/', function(req, res) {
+    res.json(_.toArray(users));
 });
 
 /* Create a new user */
-router.post('/', async function(req, res, next) {
-  try{
-  const user = await User.register(req.body);
-  //user.id = curId++;
-  const token = createToken(user);
-  return res.status(201).json({ user, token });
-} catch(err){
-  return next(err);
-}
-  // if (!user.state)
-  //   user.state = 'pending';
-  // }
-  // users[user.id] = user;
-  // log.info('Created user', user);
-  // res.json(user);
+router.post('/', function(req, res, next) {
+  var user = req.body;
+  user.id = curId++;
+  if (!user.state){
+    user.state = 'pending';
+  }
+  users[user.id] = user;
+  log.info('Created user', user);
+  res.json(user);
 });
 
 /* Get a specific user by id */
-router.get('/:id', async function(req, res, next) {
-  try{
-  var user = await User.findOne(req.params.id);
+router.get('/:id', function(req, res) {
+  var user = users[req.params.id];
   if (!user) {
-    return next();
+    return next(new Error('User not found'));
   }
-  res.json({ user });
-} catch(err){
-  return next(err);
-}
+  res.json(users[req.params.id]);
 });
 
 /* Delete a user by id */
-router.delete('/:id', async function(req, res, next) {
-  try{
-  await User.remove(req.params.id);
-  return res.json({ message: "User deleted" });
-  } catch(err){
-    return next(err);
-  }
-  });
-
-  // var user = users[req.params.id];
-  // delete users[req.params.id];
-  // res.status(204);
-  // log.info('Deleted user', user);
-  // res.json(user);
+router.delete('/:id', function(req, res) {
+  var user = users[req.params.id];
+  delete users[req.params.id];
+  res.status(204);
+  log.info('Deleted user', user);
+  res.json(user);
+}
+);
 
 /* Update a user by id */
-router.put('/:id', async function(req, res, next) {
-  try{
-  const user = await User.update(req.params.id, req.body);
-  return res.json({ user });
-  } catch(err){
-    return next(err);
-  }
-});
-/*   var user = req.body;
+router.put('/:id',function(req, res) {
+ 
+  var user = req.body;
   if (user.id != req.params.id) {
     return next(new Error('ID paramter does not match body'));
   }
   users[user.id] = user;
   log.info('Updating user', user);
   res.json(user);
-}); */
+});
 
 
 module.exports = router;
